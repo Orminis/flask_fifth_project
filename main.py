7ec59986d2e7092efb93f1102f932e30eeed6399
Kiril Nedyalkov <k.nedialkov@gmail.com>
2022-07-24 21:39:49 +0300

    importing db.py and creating user models

---
+++ main.py
@@ -0,0 +1,5 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
---
+++ models/user.py
@@ -0,0 +1,12 @@
+from db import db
+
+
+class BaseUserModel(db.Model):
+    __abstract__ = True
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    phone = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
