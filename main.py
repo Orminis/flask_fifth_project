bff2c5b717d16cb98910f6961626271bb8468118
Kiril Nedyalkov <k.nedialkov@gmail.com>
2022-07-25 15:51:49 +0300

    starting point

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from db import db
 
 app = Flask(__name__)
 api = Api(app)
+# da izwadim configuration = config("DEV_CONFIGURATION_LINK") w env a ne da q hardcodwame
+
+# конфигурацията за апп-а е във файла config
+app.config.from_object("config.DevelopmentConfig")
 migrate = Migrate(app, db)
 
 if __name__ == "main":
