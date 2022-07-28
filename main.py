55373b46e808e9d827cd608aefa64f898f7e177b
Kiril Nedyalkov <k.nedialkov@gmail.com>
2022-07-28 17:42:10 +0300

    env example file

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ from db import db
 from resources.routes import routes
 
 app = Flask(__name__)
+# Начин(метод) да се конфигурира и свърже app с db след като са инициализирани и създадени в други файлове
+db.init_app(app)
+
 # da izwadim configuration = config("DEV_CONFIGURATION_LINK") w env a ne da q hardcodwame
 # конфигурацията за апп-а е във файла config
-app.config.from_object("config.DevelopmentConfig")
+# app.config.from_object("config.DevelopmentConfig")
+app.config.from_envvar("config.FLASK_CONFIG")
 
 api = Api(app)
 migrate = Migrate(app, db)
 
 [api.add_resource(*route_data) for route_data in routes]
-
-# Начин(метод) да се конфигурира и свърже app с db след като са инициализирани и създадени в други файлове
-db.init_app(app)
 
 if __name__ == "__main__":
     app.run()
